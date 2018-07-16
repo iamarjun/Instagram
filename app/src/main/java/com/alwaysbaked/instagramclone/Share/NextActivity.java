46d802f07485e6ae5d96455d7e22170a4868f407
Arjun Manoj <arjunmanoj1995@gmail.com>
2018-07-16 22:41:28 +0530

    Setup NextActivity for Sharing image to the Feed.

--- app/src/main/java/com/alwaysbaked/instagramclone/Share/NextActivity.java
+++ app/src/main/java/com/alwaysbaked/instagramclone/Share/NextActivity.java
@@ -1,22 +1,148 @@
 package com.alwaysbaked.instagramclone.Share;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.alwaysbaked.instagramclone.R;
+import com.alwaysbaked.instagramclone.Utils.FirebaseMethods;
+import com.alwaysbaked.instagramclone.Utils.UniversalImageLoader;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class NextActivity extends AppCompatActivity {
     private static final String TAG = "NextActivity";
+
+    //constants
+    private Context mContext = NextActivity.this;
+    private static final String mAppend = "file:/";
+
+    //widgets
+    @BindView(R.id.imageShare)
+    ImageView mShareImage;
+    @BindView(R.id.caption)
+    EditText mCaption;
+    @BindView(R.id.back)
+    ImageView mBack;
+    @BindView(R.id.tvShare)
+    TextView mShare;
+
+    //firebase
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mRef;
+    private FirebaseMethods mFirebaseMethods;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next);
+        Log.d(TAG, "onCreate: starting.");
 
-        Log.d(TAG, "onCreate: got the selected image " + getIntent().getStringExtra(getString(R.string.select_image)));
+        ButterKnife.bind(this);
 
+        setupFrebaseAuth();
 
+        mBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: closing the activity.");
+                finish();
+            }
+        });
+
+        mShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: attempting to share the image to the feed.");
+
+            }
+        });
+
+    }
+
+    /**
+     * gets the image url from the incoming intent and displays the chosen image
+     */
+    private void setImage(){
+        Intent intent = getIntent();
+        if (intent.getStringExtra(getString(R.string.select_image)) != null)
+            UniversalImageLoader.setImage(intent.getStringExtra(getString(R.string.select_image)), mShareImage, null, mAppend);
+    }
+
+     /*
+    ------------------------------------------ Firebase --------------------------------------------
+     */
+
+    /**
+     * setup firebase auth object.
+     */
+
+    private void setupFrebaseAuth() {
+        Log.d(TAG, "setupFrebaseAuth: setting up firebase auth.");
+
+        mAuth = FirebaseAuth.getInstance();
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mRef = mFirebaseDatabase.getReference();
+
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                if (user != null)
+                    Log.d(TAG, "onAuthStateChanged: signed in:" + user.getUid());
+                else
+                    Log.d(TAG, "onAuthStateChanged: signed out");
+            }
+        };
+
+        mRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Log.d(TAG, "onDataChange: settings user's data");
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        mAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthStateListener != null)
+            mAuth.removeAuthStateListener(mAuthStateListener);
     }
 }
