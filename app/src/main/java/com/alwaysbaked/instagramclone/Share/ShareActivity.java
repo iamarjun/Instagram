081c40aee8a5f1c04f838c90841272b371a28484
Arjun Manoj <arjunmanoj1995@gmail.com>
2018-07-16 14:37:46 +0530

    Verifying Permissions for Sharing.

--- app/src/main/java/com/alwaysbaked/instagramclone/Share/ShareActivity.java
+++ app/src/main/java/com/alwaysbaked/instagramclone/Share/ShareActivity.java
@@ -1,8 +1,10 @@
 package com.alwaysbaked.instagramclone.Share;
 
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -10,6 +12,7 @@ import android.view.MenuItem;
 
 import com.alwaysbaked.instagramclone.R;
 import com.alwaysbaked.instagramclone.Utils.BottomNavigationViewHelper;
+import com.alwaysbaked.instagramclone.Utils.Permissions;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 import butterknife.BindView;
@@ -17,7 +20,12 @@ import butterknife.ButterKnife;
 
 public class ShareActivity extends AppCompatActivity {
     private static final String TAG = "ShareActivity";
+
+    //constants
     private static final int ACTIVITY_NUMBER = 2;
+    private static final int VERIFY_PERMISSIONS_REQUEST = 1;
+
+
     @BindView(R.id.bottomNavViewBar)
     BottomNavigationViewEx bottomNavigationViewEx;
     private Context mContext = ShareActivity.this;
@@ -29,8 +37,57 @@ public class ShareActivity extends AppCompatActivity {
         Log.d(TAG, "onCreate: staring.");
         ButterKnife.bind(this);
 
+        if (checkPermissionsArray(Permissions.PERMISSIONS)) {
+
+        } else {
+            verifyPermissions(Permissions.PERMISSIONS);
+        }
+
         setupBottomNavigationView();
 
+    }
+
+    private void verifyPermissions(String[] permissions) {
+        Log.d(TAG, "verifyPermissions: verifying permission.");
+
+        ActivityCompat.requestPermissions(ShareActivity.this, permissions, VERIFY_PERMISSIONS_REQUEST);
+    }
+
+    /**
+     * checking for an array of permission.
+     *
+     * @param permissions
+     * @return
+     */
+    private boolean checkPermissionsArray(String[] permissions) {
+        Log.d(TAG, "checkPermissionsArray: checking permissions array.");
+
+        for (String permission : permissions) {
+            if (!checkPermissions(permission)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * checking for a single permission if granted or not.
+     *
+     * @param permission
+     * @return
+     */
+    private boolean checkPermissions(String permission) {
+        Log.d(TAG, "checkPermissions: checking permission: " + permission);
+
+        int permissionRequest = ActivityCompat.checkSelfPermission(mContext, permission);
+
+        if (permissionRequest != PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, "checkPermissions: Permission Denied for: " + permission);
+            return false;
+        } else {
+            Log.d(TAG, "checkPermissions: Permission Granted for: " + permission);
+            return true;
+        }
     }
 
     /**
---
+++ app/src/main/java/com/alwaysbaked/instagramclone/Utils/Permissions.java
@@ -0,0 +1,12 @@
+package com.alwaysbaked.instagramclone.Utils;
+
+
+import android.Manifest;
+
+public class Permissions {
+    public static final String[] PERMISSIONS = {
+            Manifest.permission.CAMERA,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+}
