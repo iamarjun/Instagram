ed6dd9b3f72c366ef4f2457f134d7f6e4abfc90d
Arjun Manoj <arjunmanoj1995@gmail.com>
2018-07-16 17:00:12 +0530

    Gallery Fragment Layout.

--- app/src/main/java/com/alwaysbaked/instagramclone/Share/GalleryFragment.java
+++ app/src/main/java/com/alwaysbaked/instagramclone/Share/GalleryFragment.java
@@ -8,11 +8,32 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.alwaysbaked.instagramclone.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class GalleryFragment extends Fragment {
     private static final String TAG = "GalleryFragment";
+
+    @BindView(R.id.gridView)
+    GridView mGridView;
+    @BindView(R.id.galleryImageView)
+    ImageView mGalleryImage;
+    @BindView(R.id.progressBar)
+    ProgressBar mProgressBar;
+    @BindView(R.id.spinnerDirectory)
+    Spinner mDirectorySpinner;
+    @BindView(R.id.close)
+    ImageView mClose;
+    @BindView(R.id.tvNext)
+    TextView mNext;
 
     @Nullable
     @Override
@@ -21,6 +42,25 @@ public class GalleryFragment extends Fragment {
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_gallery, container, false);
         Log.d(TAG, "onCreateView: started");
+
+        ButterKnife.bind(this, view);
+
+        mProgressBar.setVisibility(View.GONE);
+        
+        mClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: closing the gallery fragment.");
+                getActivity().finish();
+            }
+        });
+
+        mNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: navigating to final share screen.");
+            }
+        });
 
 
         return view;
