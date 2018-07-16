8a4df1439f647d5baae1d6c5f42b4ae7b83cbb30
Arjun Manoj <arjunmanoj1995@gmail.com>
2018-07-16 15:14:00 +0530

    ShareActivity Layout and Tabs.

--- app/src/main/java/com/alwaysbaked/instagramclone/Share/ShareActivity.java
+++ app/src/main/java/com/alwaysbaked/instagramclone/Share/ShareActivity.java
@@ -4,7 +4,9 @@ import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -13,6 +15,7 @@ import android.view.MenuItem;
 import com.alwaysbaked.instagramclone.R;
 import com.alwaysbaked.instagramclone.Utils.BottomNavigationViewHelper;
 import com.alwaysbaked.instagramclone.Utils.Permissions;
+import com.alwaysbaked.instagramclone.Utils.SectionsPagerAdapter;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 import butterknife.BindView;
@@ -25,19 +28,25 @@ public class ShareActivity extends AppCompatActivity {
     private static final int ACTIVITY_NUMBER = 2;
     private static final int VERIFY_PERMISSIONS_REQUEST = 1;
 
+    private Context mContext = ShareActivity.this;
 
+    @BindView(R.id.container)
+    ViewPager mViewPager;
+    @BindView(R.id.tabsBottom)
+    TabLayout mTabLayout;
     @BindView(R.id.bottomNavViewBar)
     BottomNavigationViewEx bottomNavigationViewEx;
-    private Context mContext = ShareActivity.this;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_share);
         Log.d(TAG, "onCreate: staring.");
         ButterKnife.bind(this);
 
         if (checkPermissionsArray(Permissions.PERMISSIONS)) {
+            setupViewPager();
 
         } else {
             verifyPermissions(Permissions.PERMISSIONS);
@@ -47,6 +56,22 @@ public class ShareActivity extends AppCompatActivity {
 
     }
 
+    private void setupViewPager(){
+        SectionsPagerAdapter adapter = new SectionsPagerAdapter(getSupportFragmentManager());
+//        adapter.addFragment(new GalleryFragment());
+//        adapter.addFragment(new PhotoFragment());
+
+        mViewPager.setAdapter(adapter);
+        mTabLayout.setupWithViewPager(mViewPager);
+
+        mTabLayout.getTabAt(0).setText(R.string.gallery);
+        mTabLayout.getTabAt(1).setText(R.string.photo);
+    }
+
+    /**
+     * verify all the permission
+     * @param permissions
+     */
     private void verifyPermissions(String[] permissions) {
         Log.d(TAG, "verifyPermissions: verifying permission.");
 
